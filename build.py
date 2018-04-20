f396b3ada12075a111cdfc0f8dd5ac41a55ba0c9
Tom Seddon <tom@tmbp>
2018-04-20 19:07:53 +0100

    Add initial CI test stuff for AppVeyor.

---
+++ build.py
@@ -0,0 +1,11 @@
+import sys
+
+def main():
+    suffix=sys.argv[1]
+
+    fname='output-%s.txt'%suffix
+    print 'output name: %s'%fname
+
+    with open(fname,'wt') as f: f.write("hello. %s\n"%suffix)
+
+if __name__=="__main__": main()
